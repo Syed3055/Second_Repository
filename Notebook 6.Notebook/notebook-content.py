4f5de77662b9a1083ebeca4c733d700d6e20ca1c
Syed3055 <sajidashaik142@gmail.com>
2025-07-06 11:09:12 +0530

    new note book added to Customer table

---
+++ Notebook 6.Notebook/notebook-content.py
@@ -0,0 +1,68 @@
+# Fabric notebook source
+
+# METADATA ********************
+
+# META {
+# META   "kernel_info": {
+# META     "name": "synapse_pyspark"
+# META   },
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "0963a2d1-7145-408a-a3da-06cf3f8dea0f",
+# META       "default_lakehouse_name": "Lakehouse_01",
+# META       "default_lakehouse_workspace_id": "2baf3d08-a1ba-4127-9f42-c2ba3c1217e6",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "0963a2d1-7145-408a-a3da-06cf3f8dea0f"
+# META         }
+# META       ]
+# META     }
+# META   }
+# META }
+
+# CELL ********************
+
+from pyspark.sql.functions import *
+from pyspark.sql.types import *
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df = spark.read.format("csv").option("header","true").load("Files/customers (1).csv")
+# df now is a Spark DataFrame containing CSV data from "Files/customers (1).csv".
+display(df)
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df.write.format("delta").mode("overwrite").save("Tables/customers")
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
